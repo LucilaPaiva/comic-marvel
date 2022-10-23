6bb125da240a6884ba133867b4f94ca470fcda9a
Veronica <veronica.c.segovia@gmail.com>
2022-10-23 01:17:15 -0300

    se pintan los personajes, modo claro/oscuro funcionando, que funcione select comic/personaje con el boton buscar

---
+++ api/characters.js
@@ -0,0 +1,9 @@
+// const baseUrl = "https://gateway.marvel.com/v1/public/";
+// const apiPublic = "9793363e7276e556c84635fef3aecb00";
+
+// const getCharacters = async () =>{
+//     const response = await fetch(`${baseUrl}/characters?apikey=${apiPublic}&offset=0&orderBy=name`);
+//     const data = await response.json()
+//     return data;
+//     console.log(data);
+// }
